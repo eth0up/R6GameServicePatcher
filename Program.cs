acf3bfdc25f0e5b5a748fb00768e0efb7d23a246
eth0up <f13ftw@gmail.com>
2017-02-21 07:12:56 -0500

    removed unnecessary usings

--- Program.cs
+++ Program.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace R6GameService_Patcher
 {
